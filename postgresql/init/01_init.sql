c7f63db67368fb8807cc5175b43f1d3cfe0174bd
Nguyen Van Luan <44190998+luannv6792@users.noreply.github.com>
2025-12-16 13:13:28 +0700

    Update 01_init.sql

--- postgresql/init/01_init.sql
+++ postgresql/init/01_init.sql
@@ -1,1 +1,9 @@
 
+CREATE TABLE IF NOT EXISTS hello (
+    id SERIAL PRIMARY KEY,
+    message TEXT NOT NULL
+);
+
+INSERT INTO hello(message)
+SELECT 'Hello from PostgreSQL'
+WHERE NOT EXISTS (SELECT 1 FROM hello);
