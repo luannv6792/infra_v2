b3682cda5bc017d55523b58d57fdd0cdc37bf73b
Nguyen Van Luan <44190998+luannv6792@users.noreply.github.com>
2025-12-17 02:52:33 +0700

    Update App.jsx

--- webui/src/App.jsx
+++ webui/src/App.jsx
@@ -1,7 +1,17 @@
+import { useAuth } from "./auth/AuthContext";
 import Dashboard from "./pages/Dashboard";
 import Topbar from "./components/Topbar";
+import Login from "./pages/Login";
 
 export default function App() {
+  const { user } = useAuth();
+
+  // Chưa login → show Login page
+  if (!user) {
+    return <Login />;
+  }
+
+  // Đã login
   return (
     <>
       <Topbar />
